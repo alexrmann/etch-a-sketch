8be565f95b8e5f0e5ec67d49f6dc12455dd9c43a
Alex Mann <33494521+alexrmann@users.noreply.github.com>
2024-08-06 11:16:25 -0700

    Remove old functions except randomIndex, add comment for drawing

--- scripts.js
+++ scripts.js
@@ -64,6 +64,7 @@ drawGrid(gridSize);
 // When user clicks "Create New Grid" button
 gridBtn.addEventListener("click", newGrid);
 
+// Draw on the grid
 Array.from(gridSquares).forEach(square => square.addEventListener("mouseover", this.style.backgroundColor = 'black'))
 
 /* 
@@ -81,126 +82,14 @@ DEFINE FUNCTIONS
 */
 
 /*
-function playGame(playerChoice) {
 
-    // Log clicks to console
-    console.log("Player clicked: " + playerChoice);
-
-    // Get the computer's choice
-    const computerSelection = getComputerChoice();
-    
-    // Play out the round
-    playRound(playerChoice, computerSelection);
-
-    updateScore(playerScore, computerScore);
-
-    endGameCheck();
-    
-    // Increment to the next round
-    currentRound++;
-
-}
-
-function getComputerChoice() {
-    // Randomly select an index from choices array
-    const randomIndex = Math.floor(Math.random() * choices.length);
+function getRandomIndex() {
+    // Randomly select an index from an array
+    const randomIndex = Math.floor(Math.random() * arrayName.length);
 
     // Convert random index to corresponding string in choices array
-    console.log("Computer picked: " + choices[randomIndex]);
-    return choices[randomIndex];
+    return arrayName[randomIndex];
 }
-
-function playRound(playerChoice, computerSelection) {
-   
-    // Update the current round
-    round.textContent = `Round: ${currentRound}`;
-
-    // Announce Player's choice
-    switch(playerChoice) {
-        case playerChoice = choices[0]: 
-            textPlayer.textContent = `Player has chosen Rock`;
-            break;
-        case playerChoice = choices[1]: 
-            textPlayer.textContent = `Player has chosen Paper`;
-            break;
-        case playerChoice = choices[2]: 
-            textPlayer.textContent = `Player has chosen Scissors`;
-    }
-
-    // Announce Computer's choice
-    switch(computerSelection) {
-        case computerSelection = choices[0]: 
-            textComputer.textContent = `Computer has chosen Rock`;
-            break;
-        case computerSelection = choices[1]: 
-            textComputer.textContent = `Computer has chosen Paper`;
-            break;
-        case computerSelection = choices[2]: 
-            textComputer.textContent = `Computer has chosen Scissors`;
-    }
-
-    if (playerChoice != undefined) {
-        if (playerChoice === computerSelection) {
-            roundResult.textContent = "Tie!";
-        } else {
-            switch(true) {
-                case playerChoice === choices[0]:
-                    if (computerSelection === choices[1]) {
-                        roundResult.textContent = "You lost this round! Paper beats Rock.";
-                        computerScore++;
-                    } else { 
-                        roundResult.textContent = "You win the round! Rock beats Scissors.";
-                        playerScore++; 
-                    }
-                    break;      
-                case playerChoice === choices[1]:
-                    if (computerSelection === choices[0]) { 
-                        roundResult.textContent = "You win the round! Paper beats Rock.";
-                        playerScore++;
-                    } else {
-                        roundResult.textContent = "You lost this round! Scissors beats Paper.";
-                        computerScore++;
-                    }
-                    break;
-                case playerChoice === choices[2]:
-                    if (computerSelection === choices[0]) {
-                        roundResult.textContent = "You lost this round! Rock beats Scissors.";
-                        computerScore++;
-                    } else {
-                        roundResult.textContent = "You win the round! Scissors beats Paper.";
-                        playerScore++;
-                    } 
-            }         
-        }
-    }
-}
-
-function updateScore(playerScore, computerScore) {
-    gameStatus.textContent = `Score: Player - ${playerScore}, Computer - ${computerScore}`;
-}
-
-function endGameCheck() {
-    let score;
-    let scoreDisplay = `Player - ${playerScore}, Computer - ${computerScore}`;
-    let finalResult;
-    // Check the scores
-    if (computerScore === 5) {
-        finalResult = "Game over. Computer wins!"; 
-        score = `Final score: ${scoreDisplay}`;
-        playAgain();
-    } else if (playerScore === 5) {
-        finalResult = "Congrats! You won the game!"; 
-        score = `Final score: ${scoreDisplay}`;
-        playAgain();
-    } else {
-        score = `Score: ${scoreDisplay}`;
-    }
-
-    gameResult.textContent = finalResult;
-    gameStatus.textContent = score;
-
-}
-
 
 */
 
